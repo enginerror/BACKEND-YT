4259e336fa56aa4c9a8d9df352918165d2ce96a1
enginerror <ayaanmurshedkhan@gmail.com>
2025-12-14 11:55:20 +0530

    lecture 3

---
+++ 3/script.js
@@ -0,0 +1,20 @@
+/*
+TOPIC COVER
+1. NPM understanding
+2. install and uninstall anything basics and advanced
+3. understanding node modules
+4. dependencies
+5. devdependencies
+6. scripts - understanding default scripts PATH and custom scripts
+*/
+
+// npm - node package manager
+
+// node js core main jo installed aata hain wo kehlaata hain module
+// npm se download karte hain wo hota hain package
+
+// install - npm i packageName
+
+// dependencies - pakages and pakages ki dependencies
+
+// devdependencies - aise pakages jo sirf development main kaam aayega par jab app ban jaayega aur upload ho jaayega tab hum in packages ko use nahi kar rahe honge
